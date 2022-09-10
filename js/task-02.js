77200b997b01f436b3d520c358e04deacc43db7a
Alex-Sorokolit <sorokolital@gmail.com>
2022-09-10 19:09:17 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('ul');
+
+// Variant 1 ______________________________________
+// const items = [];
+// for (let i = 0; i < ingredients.length; i+=1) {
+// const itemEl = document.createElement('li');
+// itemEl.textContent = ingredients[i];
+// itemEl.classList.add('item')
+// items.push(itemEl);
+// };
+
+// Variant 2 ______________________________________
+const items = ingredients.map(option => {
+  const item = document.createElement('li');
+  item.textContent = option;
+  item.classList.add('item')
+  return item;
+})
+listEl.append(...items);
