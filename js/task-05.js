cd6d6d4309e46b278d2b79e8825da68a5355d35e
Alex-Sorokolit <sorokolital@gmail.com>
2022-09-11 12:45:52 +0300

    task-5

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector('input')
+const userNameEl = document.querySelector('#name-output')
+
+inputEl.addEventListener('input', onInputChange);
+function onInputChange(event) {
+ userNameEl.textContent = event.currentTarget.value
+  ? event.currentTarget.value
+  : 'Anonymous'; 
+};
