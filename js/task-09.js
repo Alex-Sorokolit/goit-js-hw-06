cb2b937c99fb6612489c36706f05af3a15660fa2
Alex-Sorokolit <sorokolital@gmail.com>
2022-09-11 22:55:35 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector('.change-color')
+console.log(buttonEl);
+
+const textEl = document.querySelector('.color')
+console.log(textEl);
+
+
+const changeColor = () => {
+  const color = getRandomHexColor()
+  document.body.style.backgroundColor = color;
+  textEl.textContent = color;
+}
+
+buttonEl.addEventListener('click', changeColor)
