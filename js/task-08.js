87d051298768b71de24fa1ff3555ab5cc8169d21
Alex-Sorokolit <sorokolital@gmail.com>
2022-09-11 21:32:15 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+// get Form
+const formEl = document.querySelector('.login-form')
+// get mail input
+const mailEl = formEl.firstElementChild
+// get password input
+const passwordEl = mailEl.nextElementSibling
+// get Button
+const loginBtnEl = document.querySelector('button')
+console.log(loginBtnEl);
+
+
+function handleSubmit(event){
+ event.preventDefault();
+ console.log('event', event);
+
+ if (event.target.email.value === "" || event.target.password.value === "") {
+  alert('Bсі поля повинні бути заповнені')
+  return
+ }
+
+ const userData = {
+  email: event.target.email.value,
+  password: event.target.password.value,
+ }
+
+ console.log(userData);
+ formEl.reset();
+};
+formEl.addEventListener('submit', handleSubmit);
 
